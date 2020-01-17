6950c2d51f24ea4bbe2a0bbb9a6320cdfc07e2cd
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-01-17 16:04:13 +0100

    create what-we-do page

--- src/pages/focus.js
+++ src/pages/focus.js
@@ -1,15 +1,126 @@
 import React from "react"
-import { Link } from "gatsby"
+import { startupSupport } from "../images/focus"
 
 import Layout from "../components/layout"
+import FocusStyle from "../styles/focus.module.scss"
 import SEO from "../components/seo"
+import { FaSuperpowers, FaCode, FaChalkboardTeacher, FaProjectDiagram, FaUserCircle } from "react-icons/fa"
+import { MdBuild, MdAccessible } from "react-icons/md"
 
 const AreasOfFocusPage = () => (
   <Layout>
-    <SEO title="Focus" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Our Focus" />
+    <div className={FocusStyle.container}>
+      <h2 className={FocusStyle.header}>
+        {" "}
+        We help to catalyze Africa's next generation innovations.
+      </h2>
+      <section className={FocusStyle.mainSection}>
+        <h3 className={FocusStyle.mainSectionHeader}>Areas of Focus</h3>
+        <div className={FocusStyle.cardContainer}>
+          <div className={FocusStyle.card}>
+            <div className={FocusStyle.cardIcon}>
+              <FaSuperpowers className={FocusStyle.focusIcon} />
+            </div>
+            <div className={FocusStyle.focusArea}>
+              <h3 className={FocusStyle.focusHeader}>We Support Startups</h3>
+              <p className={FocusStyle.focusText}>
+                Duis sint elit minim magna aute ipsum amet tempor enim eu dolore
+                reprehenderit. Cillum excepteur pariatur culpa eu excepteur
+                irure. Elit pariatur anim irure cillum ullamco laborum sit
+              </p>
+            </div>
+          </div>
+
+          <div className={FocusStyle.card}>
+            <div className={FocusStyle.cardIcon}>
+              <FaCode className={FocusStyle.focusIcon} />
+            </div>
+            <div className={FocusStyle.focusArea}>
+              <h3 className={FocusStyle.focusHeader}>
+                Software Development and Outsourcing
+              </h3>
+              <p className={FocusStyle.focusText}>
+                Duis sint elit minim magna aute ipsum amet tempor enim eu dolore
+                reprehenderit. Cillum excepteur pariatur culpa eu excepteur
+                irure. Elit pariatur anim irure cillum ullamco laborum sit
+              </p>
+            </div>
+          </div>
+
+          <div className={FocusStyle.card}>
+            <div className={FocusStyle.cardIcon}>
+              <FaChalkboardTeacher className={FocusStyle.focusIcon} />
+            </div>
+            <div className={FocusStyle.focusArea}>
+              <h3 className={FocusStyle.focusHeader}>Capacity building</h3>
+              <p className={FocusStyle.focusText}>
+                Duis sint elit minim magna aute ipsum amet tempor enim eu dolore
+                reprehenderit. Cillum excepteur pariatur culpa eu excepteur
+                irure. Elit pariatur anim irure cillum ullamco laborum sit
+              </p>
+            </div>
+          </div>
+
+          <div className={FocusStyle.card}>
+            <div className={FocusStyle.cardIcon}>
+              <MdAccessible className={FocusStyle.focusIcon} />
+            </div>
+            <div className={FocusStyle.focusArea}>
+              <h3 className={FocusStyle.focusHeader}>Market Entry</h3>
+              <p className={FocusStyle.focusText}>
+                Duis sint elit minim magna aute ipsum amet tempor enim eu dolore
+                reprehenderit. Cillum excepteur pariatur culpa eu excepteur
+                irure. Elit pariatur anim irure cillum ullamco laborum sit
+              </p>
+            </div>
+          </div>
+
+          <div className={FocusStyle.card}>
+            <div className={FocusStyle.cardIcon}>
+              <FaProjectDiagram className={FocusStyle.focusIcon} />
+            </div>
+            <div className={FocusStyle.focusArea}>
+              <h3 className={FocusStyle.focusHeader}>Consulting and Project Implementation</h3>
+              <p className={FocusStyle.focusText}>
+                Duis sint elit minim magna aute ipsum amet tempor enim eu dolore
+                reprehenderit. Cillum excepteur pariatur culpa eu excepteur
+                irure. Elit pariatur anim irure cillum ullamco laborum sit
+              </p>
+            </div>
+          </div>
+
+          <div className={FocusStyle.card}>
+            <div className={FocusStyle.cardIcon}>
+              <FaUserCircle className={FocusStyle.focusIcon} />
+            </div>
+            <div className={FocusStyle.focusArea}>
+              <h3 className={FocusStyle.focusHeader}>Social Impact</h3>
+              <p className={FocusStyle.focusText}>
+                Duis sint elit minim magna aute ipsum amet tempor enim eu dolore
+                reprehenderit. Cillum excepteur pariatur culpa eu excepteur
+                irure. Elit pariatur anim irure cillum ullamco laborum sit
+              </p>
+            </div>
+          </div>
+
+          <div className={FocusStyle.card}>
+            <div className={FocusStyle.cardIcon}>
+              <FaChalkboardTeacher className={FocusStyle.focusIcon} />
+            </div>
+            <div className={FocusStyle.focusArea}>
+              <h3 className={FocusStyle.focusHeader}>WorkSpace Rental</h3>
+              <p className={FocusStyle.focusText}>
+                Duis sint elit minim magna aute ipsum amet tempor enim eu dolore
+                reprehenderit. Cillum excepteur pariatur culpa eu excepteur
+                irure. Elit pariatur anim irure cillum ullamco laborum sit
+              </p>
+            </div>
+          </div>
+
+        </div>
+      </section>
+    </div>
   </Layout>
 )
 
