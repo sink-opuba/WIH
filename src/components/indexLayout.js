d76bc6480d42cf39febe09dbe51d08347753190f
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-01-17 11:53:59 +0100

    extract indexLayout component

---
+++ src/components/indexLayout.js
@@ -0,0 +1,29 @@
+
+import React from "react"
+import PropTypes from "prop-types"
+
+import Header from "./header"
+import Navigation from "./navigation"
+import Logo from "../images/waffiihub-logo-white.png"
+import "../styles/global.scss"
+import layoutStyles from "./indexLayout.module.scss"
+
+const IndexLayout = ({ children }) => {
+  return (
+    <>
+      <div className={layoutStyles.container}>
+        <Navigation />
+        <Header Logo={Logo} />
+        <div>
+          <main>{children}</main>
+        </div>
+      </div>
+    </>
+  )
+}
+
+IndexLayout.propTypes = {
+  children: PropTypes.node.isRequired,
+}
+
+export default IndexLayout;
