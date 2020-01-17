8143a182e4c2099ce6d483f9b02a83b886493d78
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-01-17 11:53:21 +0100

    set up about us page

---
+++ src/images/team/index.js
@@ -0,0 +1,7 @@
+import user2 from "./user-2.jpg"
+import user3 from "./user-3.jpg"
+import user4 from "./user-4.jpg"
+import user5 from "./user-5.jpg"
+import user6 from "./user-6.jpg"
+
+export { user2, user3, user4, user5, user6 }
